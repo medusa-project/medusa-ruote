f7666668077f1863667ae6b7f07da8a0c150bccd
Howard Ding <hding2@illinois.edu>
2011-05-12 08:15:27 -0700

    Still working on getting things to daemonize. The amqp-worker now seems to do this properly (although now I need to add proper logging and so on and have some more experiments to do), and it suggests a possibility for the main script.

--- amqp-worker.rb
+++ amqp-worker.rb
@@ -9,37 +9,41 @@ require 'bunny'
 require 'yajl'
 require 'yajl/json_gem'
 require 'open3'
+require 'daemons'
 
-EventMachine.run do
-  MQ.queue('make_file_types').subscribe do |workitem|
-    h = JSON.parse(workitem)
-    puts "Received workitem #{workitem}:"
-    puts JSON::pretty_generate(h)
-    dir = h['fields']['dir']
-    types = Dir[File.join('processing', dir, '*')].collect do |filename|
-      nil if ['.', '..'].include?(filename)
-      filetype = Open3.popen3('file', '-b', filename) do |stdin, stdout, stderr|
-        stdout.read
+working_dir = Dir.getwd
+Daemons.run_proc('amqp-worker.rb') do
+  EventMachine.run do
+    MQ.queue('make_file_types').subscribe do |workitem|
+      Dir.chdir working_dir
+      h = JSON.parse(workitem)
+      puts "Received workitem #{workitem}:"
+      puts JSON::pretty_generate(h)
+      dir = h['fields']['dir']
+      types = Dir[File.join('processing', dir, '*')].collect do |filename|
+        nil if ['.', '..'].include?(filename)
+        filetype = Open3.popen3('file', '-b', filename) do |stdin, stdout, stderr|
+          stdout.read
+        end
+        #filetype = `file -b #{filename}`
+        [File.basename(filename), filetype]
       end
-      #filetype = `file -b #{filename}`
-      [File.basename(filename), filetype]
-    end
-    File.open(File.join('processing', dir, 'file_types'), 'w') do |f|
-      puts "File info:"
-      types.each do |file_info|
-        name, type = *file_info
-        output = "#{name}:\t#{type}"
-        puts output
-        f.puts(output)
+      File.open(File.join('processing', dir, 'file_types'), 'w') do |f|
+        puts "File info:"
+        types.each do |file_info|
+          name, type = *file_info
+          output = "#{name}:\t#{type}"
+          puts output
+          f.puts(output)
+        end
       end
+      bunny = Bunny.new
+      bunny.start
+      return_queue = bunny.queue('ruote_workitems')
+      return_queue.publish(workitem)
     end
-    bunny = Bunny.new
-    bunny.start
-    return_queue = bunny.queue('ruote_workitems')
-    return_queue.publish(workitem)
   end
 end
-
 # The received workitem is just a json object which is a hash (the workitem) converted to JSON
 # for example:
 #{"participant_name":"make_file_types","fields":{"dir":"test-items","dispatched_at":"2011-05-11 17:16:42.915043 UTC","params":{"participant_options":{"forget":false,"queue":"make_file_types"},"ref":"make_file_types"}},"fei":{"wfid":"20110511-binotzujido","engine_id":"engine","expid":"0_0_2","subid":"13a7628bb33dbd68604cb511bafe60e3"}}
--- workflow.rb
+++ workflow.rb
@@ -9,8 +9,10 @@ require 'digest'
 require 'ruote-amqp'
 require 'yajl'
 require 'yajl/json_gem'
+require 'daemons'
 
 engine = Ruote::Engine.new(Ruote::Worker.new(Ruote::FsStorage.new('ruote-storage')))
+
 FileUtils.mkdir_p(['in', 'out', 'processing'])
 
 class MoveToProcessingParticipant
@@ -80,6 +82,22 @@ end
 #and on the child we'll start another event machine that watches directories or does whatever else
 #to kick off workflows.
 #If ultimately there is a problem with this we can go back to using loop
+
+#Also - I'm having problems getting this to daemonize this way. Obviously I'd like to use something
+#like Daemons to make it as simple as possible, but the normal patterns seem to fail (perhaps because of the
+#fork).
+#So as another idea, what would happen if we had another script that only ran
+#RuoteAMQP::Receiver and did nothing else (so it would define an engine on the same storage and
+#just use it to pass along workitems that it got).
+#It's possible that would create synchronization problems of some sort - but it's also possible that
+#it wouldn't. It does appear that the FsStorage uses locks to prevent collisions of workers from a single
+#engine, and I'd think that would work with more than one engine.
+#Some research suggests that this may be possible. A possible problem is if 'engine level' variables are stored,
+#but I don't know exactly what this means in a practical sense (also that thread was old and the problem
+#may be solved).
+#It may also be some other sort of problem with Daemons - there's a lot of fiddly stuff you need to make
+#sure you get right involving the working directory, streams, etc. I need to review some other stuff I've done
+#with it to see if that provides any hints. 
 pid = Kernel.fork
 if (pid)
   RuoteAMQP::Receiver.new(engine)
@@ -96,4 +114,3 @@ else
     end
   end
 end
-
