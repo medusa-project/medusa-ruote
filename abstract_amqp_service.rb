30242f82249c34c676c1960e5369be294ea4a008
Howard Ding <hading2@gmail.com>
2011-05-12 15:32:53 -0500

    Making progress on abstracting out common portions of what an amqp service will do.

--- abstract_amqp_service.rb
+++ abstract_amqp_service.rb
@@ -1,5 +1,94 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'yajl'
+require 'yajl/json_gem'
+require 'log4r'
+require 'bunny'
+require 'mq'
+require 'daemons'
+require 'fileutils'
+
 class AbstractAMQPService < Object
+  def start
+    working_dir = Dir.getwd
+    ensure_log_and_pid_dirs()
+    Daemons.run_proc(self.process_name, :dir => self.pid_dir,
+                     :log_dir => self.log_dir, :backtrace => true) do
+      EventMachine.run do
+        Dir.chdir working_dir
+        start_logging()
+        listen()
+      end
+    end
+  end
 
+  def ensure_log_and_pid_dirs
+    [self.log_dir, self.pid_dir].each { |dir| FileUtils.mkdir_p(dir) }
+  end
 
+  def start_logging
+    self.setup_logger
+    self.logger.info "Started"
+    Kernel.at_exit do
+      self.logger.info "Stopped"
+    end
+  end
+
+  def listen
+    MQ.queue(self.amqp_listen_queue, :durable => true).subscribe do |workitem|
+      h = process_workitem(JSON.parse(workitem))
+      self.reply_to_engine(h || workitem)
+    end
+  end
+
+  attr_accessor :logger
+
+  def setup_logger
+    self.logger = Log4r::Logger.new('log')
+    outputter = Log4r::RollingFileOutputter.new('log', :filename => File.join('log', self.service_name),
+                                                :maxtime => (3600 * 24), :max_backups => 6)
+    self.logger.outputters = outputter
+    outputter.formatter = Log4r::PatternFormatter.new(:pattern => "%d [%5l] %M")
+  end
+
+  def service_name
+    raise RuntimeError, 'Subclass responsibility'
+  end
+
+  def process_name
+    "#{self.service_name}.rb"
+  end
+
+  def log_dir
+    'log'
+  end
+
+  def pid_dir
+    'pid'
+  end
+
+  #Override in subclasses to do the work for each message received.
+  #Receives the workitem parsed into a Ruby hash.
+  #Should return a Ruby hash to be turned back into a JSON work item
+  #that has any needed modifications done to it.
+  #If there is no needed modification you can return false and
+  #the original workitem will be returned
+  def process_workitem(h)
+    h
+  end
+
+  def amqp_return_queue
+    'ruote_workitems'
+  end
+
+  #Take the workitem (as a hash or a JSON string) and return it to
+  #the workflow engine
+  def reply_to_engine(workitem)
+    workitem = workitem.to_json if workitem.is_a?(Hash)
+    bunny = Bunny.new
+    bunny.start
+    return_queue = bunny.queue(self.amqp_return_queue, :durable => true)
+    return_queue.publish(workitem, :persistent => true)
+  end
 
 end
--- amqp_worker.rb
+++ amqp_worker.rb
@@ -4,61 +4,41 @@
 
 require 'rubygems'
 require 'bundler/setup'
-require 'mq'
-require 'bunny'
-require 'yajl'
-require 'yajl/json_gem'
 require 'open3'
-require 'daemons'
-require 'log4r'
-require 'fileutils'
 require 'abstract_amqp_service'
 
 class AMQPWorker < AbstractAMQPService
-  def start
-    working_dir = Dir.getwd
-    FileUtils.mkdir_p('log')
-    FileUtils.mkdir_p('pid')
-    Daemons.run_proc('amqp_worker.rb', :dir => 'pid', :log_dir => 'log', :backtrace => true) do
-      EventMachine.run do
-        Dir.chdir working_dir
-        logger = Log4r::Logger.new('log')
-        logger.outputters = outputter = Log4r::RollingFileOutputter.new('log', :filename => 'log/amqp-worker', :maxtime => (3600 * 24), :max_backups => 6)
-        outputter.formatter = Log4r::PatternFormatter.new(:pattern => "%d [%5l] %M")
-        logger.info "Started"
-        MQ.queue('make_file_types', :durable => true).subscribe do |workitem|
-          h = JSON.parse(workitem)
-          logger.info "Received workitem #{workitem}:"
-          logger.info JSON::pretty_generate(h)
-          dir = h['fields']['dir']
-          types = Dir[File.join('processing', dir, '*')].collect do |filename|
-            nil if ['.', '..'].include?(filename)
-            filetype = Open3.popen3('file', '-b', filename) do |stdin, stdout, stderr|
-              stdout.read
-            end
-            #filetype = `file -b #{filename}`
-            [File.basename(filename), filetype]
-          end
-          File.open(File.join('processing', dir, 'file_types'), 'w') do |f|
-            logger.info "File info:"
-            types.each do |file_info|
-              name, type = *file_info
-              output = "#{name}:\t#{type}"
-              logger.info output
-              f.puts(output)
-            end
-          end
-          bunny = Bunny.new
-          bunny.start
-          return_queue = bunny.queue('ruote_workitems', :durable => true)
-          return_queue.publish(workitem, :persistent => true)
-        end
-        Kernel.at_exit do
-          logger.info "Stopped"
-        end
+
+  def service_name
+    'amqp_worker'
+  end
+
+  def amqp_listen_queue
+    'make_file_types'
+  end
+
+  def process_workitem(h)
+    dir = h['fields']['dir']
+    types = Dir[File.join('processing', dir, '*')].collect do |filename|
+      nil if ['.', '..'].include?(filename)
+      filetype = Open3.popen3('file', '-b', filename) do |stdin, stdout, stderr|
+        stdout.read
+      end
+      #filetype = `file -b #{filename}`
+      [File.basename(filename), filetype]
+    end
+    File.open(File.join('processing', dir, 'file_types'), 'w') do |f|
+      self.logger.info "File info:"
+      types.each do |file_info|
+        name, type = *file_info
+        output = "#{name}:\t#{type}"
+        self.logger.info output
+        f.puts(output)
       end
     end
+    nil
   end
+
 end
 AMQPWorker.new.start
 # The received workitem is just a json object which is a hash (the workitem) converted to JSON
