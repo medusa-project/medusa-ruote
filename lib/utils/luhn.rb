b87952f4cf2c3c8eb9a11b10bb3f1e81bb2a84af
Howard Ding <hding2@illinois.edu>
2011-05-24 13:17:13 -0700

    Reorganized as a class and added method to add check digit to a string.

--- lib/utils/luhn.rb
+++ lib/utils/luhn.rb
@@ -1,25 +1,9 @@
 #c.f. http://en.wikipedia.org/wiki/Luhn_mod_N_algorithm
+#this is specialized to handle the case where our string represents a hexadecimal number
 
-module Luhn
+class Luhn
 
-  module_function
-
-  CHAR_TO_CODE_MAP = (('a'..'f').to_a + ('0'..'9').to_a).inject({}) {|acc, char| acc[char] = char.hex; acc}
-  CODE_TO_CHAR_MAP = CHAR_TO_CODE_MAP.invert
-
-  def code_point_to_character(code_point)
-    CODE_TO_CHAR_MAP[code_point]
-  end
-
-  def character_to_code_point(char)
-    CHAR_TO_CODE_MAP[char]
-  end
-
-  def number_of_valid_input_characters
-    16
-  end
-
-  def check_character(string)
+  def self.check_character(string)
     factor = 2
     sum = 0
     n = number_of_valid_input_characters
@@ -35,13 +19,34 @@ module Luhn
     code_point_to_character(check_code_point)
   end
 
-  def verify(string_plus_check_digit)
+  def self.add_check_character(string, separator = '-')
+    string + separator + check_character(string)
+  end
+
+  def self.verify(string_plus_check_digit)
     string = strip_string(string_plus_check_digit)
-    check_digit = string.slice!(-1,1)
+    check_digit = string.slice!(-1, 1)
     check_digit == check_character(string)
   end
 
-  def strip_string(string)
+  protected
+
+  CHAR_TO_CODE_MAP = (('a'..'f').to_a + ('0'..'9').to_a).inject({}) { |acc, char| acc[char] = char.hex; acc }
+  CODE_TO_CHAR_MAP = CHAR_TO_CODE_MAP.invert
+
+  def self.code_point_to_character(code_point)
+    CODE_TO_CHAR_MAP[code_point]
+  end
+
+  def self.character_to_code_point(char)
+    CHAR_TO_CODE_MAP[char]
+  end
+
+  def self.number_of_valid_input_characters
+    16
+  end
+
+  def self.strip_string(string)
     string.gsub(/[^0-9a-f]/, '')
   end
 
