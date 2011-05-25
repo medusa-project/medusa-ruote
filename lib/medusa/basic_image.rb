18bdabe232dceb70e91dad0fe7e7b2e0e7eaf26e
Bill Ingram <waingram@gmail.com>
2011-05-25 17:39:38 -0500

    Adds medusa image and collection objects. First pass at ingest service finished (not tested).

---
+++ lib/medusa/basic_image.rb
@@ -0,0 +1,25 @@
+module Medusa
+
+  class BasicImage < ActiveFedora::Base
+
+    # Uses the Hydra Rights Metadata Schema for tracking access permissions & copyright
+    #has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata
+
+    #has_metadata :name => "descMetadata", :type => Hydra::ModsImage
+
+    #has_metadata :name => 'preservationMetadata', :type => Medusa::Premis
+
+    # A place to put extra metadata values
+    has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
+      m.field 'collection', :string
+      m.field 'depositor', :string
+      m.field 'title', :string
+    end
+
+    def initialize(attrs={})
+      super(attrs)
+      #add_relationship(:has_model, "hydra-cModel:commonMetadata")
+      #add_relationship(:has_model, "hydra-cModel:genericContent")
+    end
+  end
+end
