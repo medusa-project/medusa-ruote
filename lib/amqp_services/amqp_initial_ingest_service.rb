0df8ece2ee9b18c79cd4e388ffff546add46024c
Howard Ding <hding2@illinois.edu>
2011-05-27 11:34:59 -0700

    Refactor a bit.

--- lib/amqp_services/amqp_initial_ingest_service.rb
+++ lib/amqp_services/amqp_initial_ingest_service.rb
@@ -19,32 +19,30 @@ class AMQPInitialIngestService < AbstractFedoraAMQPService
 
   def process_workitem(workitem)
     with_parsed_workitem(workitem) do |h|
+      begin
+        #create at new bag
+        bag = extract_bag(h['fields']['dir'])
 
-      #create at new bag
-      bag = extract_bag(h['fields']['dir'])
+        #add uuid to workitem
+        h['fields']['uuid'] = Luhn.add_check_character(UUID.generate)
 
-      #generate uuid for object
-      uuid = Luhn.add_check_character(UUID.generate)
+        #use bag to create fedora object of type Medusa::BasicImage using uuid
+        bag_to_fedora_object(bag, h['fields']['uuid'], Medusa::BasicImage)
 
-      #add uuid to workitem
-      h['fields']['uuid'] = uuid
-
-      #use bag to create fedora object of type Medusa::BasicImage using uuid
-      bag_to_fedora_object(bag, uuid, Medusa::BasicImage)
-
-      #return modified workitem
-      return h
+        #return modified workitem
+        return h
 
       #TODO this should probably do something that will indicate to the engine that
       #the step failed. We need to research that some more - there may be an
       #idiomatic way to do it.
       rescue InvalidBagError => e
-      h['fields']['errors'] << "#{e.class}: #{e.message}"
-      return h
+        h['fields']['errors'] << "#{e.class}: #{e.message}"
+        return h
       #TODO we should rescue other errors, say if making the fedora object fails
       rescue Exception => e
-      h['fields']['errors'] << "#{e.class} #{e.message}"
-      return h
+        h['fields']['errors'] << "#{e.class} #{e.message}"
+        return h
+      end
     end
   end
 
