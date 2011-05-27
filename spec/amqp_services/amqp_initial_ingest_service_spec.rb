82e7568d0d25740e90103dab3d927dabaa298c46
Howard Ding <hding2@illinois.edu>
2011-05-27 13:34:48 -0700

    Added specs for bag interaction.

--- spec/amqp_services/amqp_initial_ingest_service_spec.rb
+++ spec/amqp_services/amqp_initial_ingest_service_spec.rb
@@ -13,5 +13,20 @@ describe AMQPInitialIngestService do
     AMQPInitialIngestService.new.service_name.should == 'amqp_initial_ingest_service'
   end
 
+  describe "bag interaction" do
+    before(:each) do
+      @service = AMQPInitialIngestService.new
+    end
 
+    it "should be able to extract a bag from a directory" do
+      bag = @service.extract_bag(File.join(PROJECT_ROOT, 'test-items', 'bag0'))
+      bag.valid?.should be_true
+      bag.should be_a(BagIt::Bag)
+    end
+
+    it "should error if it tries to extract a bag from a directory that isn't a bag" do
+      lambda {@service.extract_bag(File.join(PROJECT_ROOT, 'test-items', 'bag-invalid'))}.should raise_error(InvalidBagError)
+    end
+
+  end
 end
