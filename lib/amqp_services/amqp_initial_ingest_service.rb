9a806e14a7f3aa8edc80bedbadf41196fa4af8ae
Howard Ding <hding2@illinois.edu>
2011-05-27 13:33:46 -0700

    Add namespace BagIt to Bag reference.

--- lib/amqp_services/amqp_initial_ingest_service.rb
+++ lib/amqp_services/amqp_initial_ingest_service.rb
@@ -48,7 +48,7 @@ class AMQPInitialIngestService < AbstractFedoraAMQPService
 
   #create a Bag, throwing an exception if there is a problem with the incoming package
   def extract_bag(dir)
-    bag = Bag.new(dir)
+    bag = BagIt::Bag.new(dir)
     unless bag.valid?
       message = "Invalid bag at: #{dir}"
       logger.error(message)
