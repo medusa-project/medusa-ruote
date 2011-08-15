d44202e09d5fc9daa1ca77301e479df4d07ea2d0
Howard Ding <hding2@illinois.edu>
2011-08-15 12:25:37 -0700

    Corrected specs for some refactoring that has happened.

---
+++ spec/services/amqp_initial_ingest_service_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'lib/services/amqp_initial_ingest_service'
+
+describe AMQPInitialIngestService do
+
+  it { should be_a(AbstractFedoraAMQPService) }
+
+  it "should listen to the initial_ingest_service queue" do
+    AMQPInitialIngestService.amqp_listen_queue.should == 'initial_ingest_service'
+  end
+
+  it "should be be named amqp_initial_ingest_service" do
+    AMQPInitialIngestService.new.service_name.should == 'amqp_initial_ingest_service'
+  end
+
+
+end
