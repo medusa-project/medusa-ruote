ced4a8cd85dd0a6cb6239e41d8fe16dfdbbbb01a
Howard Ding <hding2@illinois.edu>
2011-08-15 14:36:23 -0700

    Make sure that the main server starts up first.

--- script/services_runner.rb
+++ script/services_runner.rb
@@ -6,7 +6,12 @@ unless action = ARGV[0]
   puts "USAGE: #{$PROGRAM_NAME} < start | stop | restart | run | ... > (see Daemons gem for all actions)"
   exit
 end
+#Make sure the main server starts first
+puts "Executing main server #{action}"
+system('lib/runners/server_runner.rb', action)
+#Then run the other servers
 Filescan.new('lib/runners', true, false).each_filename do |name|
+  next if name == 'server_runner.rb'
   puts "Executing #{name} #{action}"
   system(name, action)
 end
