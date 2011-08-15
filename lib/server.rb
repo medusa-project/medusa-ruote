9bf3338c0e3906bca23248fad032d3a056feecdc
Howard Ding <hding2@illinois.edu>
2011-08-15 11:06:33 -0700

    Refactored a bit. Make the config smoother and make everything that gets configured actually part of the object so that we never need to refer to the config hash again.

--- lib/server.rb
+++ lib/server.rb
@@ -14,11 +14,15 @@ require 'lib/utils/bag_utils'
 
 class MedusaServer
 
-  attr_accessor :config
+  attr_accessor :working_dir, :incoming_dir, :ready_dir, :processing_dir
+  attr_accessor :poll_frequency, :incoming_dir_processing_delay
+
+  def initialize
+    self.working_dir = Dir.getwd
+    read_config(working_dir)
+  end
 
   def start
-    working_dir = Dir.getwd
-    read_config(working_dir)
     Daemons.run_proc('medusa_server.rb', :dir => 'pid', :log => 'log',
                      :backtrace => true) do
       Dir.chdir(working_dir)
@@ -40,18 +44,14 @@ class MedusaServer
 
   protected
 
-  def read_config(base_dir)
-    self.config = YAML.load_file(File.join(base_dir, 'config', 'server.yml'))
-  end
-
   #Check the incoming directory for valid bags. Move valid bags to the ready
   #directory.
   def start_incoming_bag_checker
     start_periodic_timer_with_mutex() do
-      Filescan.new(self.config['incoming_directory'], false, false).each_dirname do |dir_name|
+      Filescan.new(self.incoming_dir, false, false).each_dirname do |dir_name|
         #make sure files have been unmodified for a while. Not strictly necessary, but cheaper
         #than checking the whole bag each time while it's still being copied in.
-        next unless directory_unmodified?(dir_name, self.config['incoming_directory_processing_delay'])
+        next unless directory_unmodified?(dir_name, self.incoming_dir_processing_delay)
         #make sure we have a valid bag
         begin
           next unless BagUtils.extract_bag(dir_name)
@@ -62,7 +62,7 @@ class MedusaServer
           #or something.
         end
         #Here we have a good bag. Move it to the ready directory.
-        FileUtils.mv(dir_name, File.join(self.config['ready_directory'], File.basename(dir_name)))
+        FileUtils.mv(dir_name, File.join(self.ready_dir, File.basename(dir_name)))
       end
     end
   end
@@ -99,7 +99,7 @@ class MedusaServer
   #run a block under a periodic timer but with a mutex.
   #If the timer is called but the mutex is locked then just skip the block
   #for that iteration.
-  def start_periodic_timer_with_mutex(poll_frequency = self.config['poll_frequency'])
+  def start_periodic_timer_with_mutex(poll_frequency = self.poll_frequency)
     mutex = Mutex.new
     EventMachine::PeriodicTimer.new(poll_frequency.to_i) do
       if mutex.try_lock
@@ -112,4 +112,16 @@ class MedusaServer
     end
   end
 
+  def read_config(base_dir)
+    config = YAML.load_file(File.join(base_dir, 'config', 'server.yml'))
+    self.incoming_dir = working_subdir(config['incoming_directory'] || 'items_incoming')
+    self.ready_dir = working_subdir(config['ready_directory'] || 'items_ready')
+    self.processing_dir = working_subdir(config['processing_directory'] || 'items_processing')
+    self.poll_frequency = config['poll_frequency'] || 5
+    self.incoming_dir_processing_delay = config['incoming_directory_processing_delay'] || 5
+  end
+
+  def working_subdir(dirname)
+    File.join(self.working_dir, dirname)
+  end
 end
