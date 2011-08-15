034c777b8d1f019d8e0854a38c8e61e63d406ef2
Howard Ding <hding2@illinois.edu>
2011-08-15 14:18:59 -0700

    Hooked up collection creation process in a very crude way.

--- lib/process_launcher.rb
+++ lib/process_launcher.rb
@@ -4,12 +4,20 @@ class ProcessLauncher
 
   #Answer a Process if we know what to do with this bag, nil otherwise
   def process_for(bag)
-
+    requested_process = bag.bag_info('Medusa-Process')
+    case requested_process
+      when 'create-collection'
+        CreateCollectionProcess
+      else
+        nil
+    end
   end
 
   #Process the bag - actually record the directory and start the process in the engine
   def process_bag(bag, bag_directory)
-    MedusaEngine.instance.engine.launch(self.process_for(bag), :processing_dir => bag_directory)
+    if process = self.process_for(bag)
+      MedusaEngine.instance.engine.launch(process.process_definition, :processing_dir => bag_directory)
+    end
   end
 
 end
