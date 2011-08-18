da06bd8ac0c69aa0ab1c28267faa9bf750fb9a23
Howard Ding <hding2@illinois.edu>
2011-08-18 13:15:48 -0700

    Make the demo pull the simple demo items directory.

--- demo/start-demo.sh
+++ demo/start-demo.sh
@@ -8,5 +8,5 @@ mkdir 'in' out processing
 lib/demo/demo_server.rb restart
 lib/demo/runners/amqp_file_type_service_runner.rb restart
 sleep 5
-cp -a test-items in/.
-mv in/test-items in/test-items_ready
+cp -a test-items/demo-items in/test-items
+mv in/test-items/ in/test-items_ready
